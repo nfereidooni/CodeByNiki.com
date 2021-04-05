cc6855fb859bc466842ca9307b58b6acd82c67dd
Niki Fereidooni <nikifereidooni@gmail.com>
2021-04-04 22:33:46 -0400

    minor updates

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,7 @@ function Footer(props) {
       <div className="container-sm">
         <div className="row">
           <footer className="col-12 py-4 mt-5 text-light fixed-bottom bg-danger justify-content-center text-center">
-            <p>&#169; Copyright 2020, Created by Niki Fereidooni using React</p>
+            <p>&#169; Copyright 2021, Created by Niki Fereidooni using React</p>
           </footer>
         </div>
       </div>
