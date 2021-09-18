ce140819e428c5ae7ff28f680951921107eeb913
Niki Fereidooni <nikifereidooni@gmail.com>
2021-09-18 13:03:54 -0400

    Updated styling, contact page, and removed unused images

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.css"
 import "./App.css";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
--- src/components/Design/index.js
+++ src/components/Design/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import "./style.css"
-import getgrowing from "../assets/img/gardenapp.png"
-import restaurantfinder from "../assets/img/restaurantfinder.PNG"
-import flamegame from "../assets/img/flamegame.PNG"
-import codequiz from "../assets/img/codequiz.PNG"
-import eatdaburger from "../assets/img/eatdaburger.PNG"
-import workdayscheduler from "../assets/img/workdayscheduler.PNG"
-import weatherdashboard from "../assets/img/weathedashboard.PNG"
 import { Container } from "react-bootstrap";
-import Project from "../Project"
 
 
 function Design(props) {
