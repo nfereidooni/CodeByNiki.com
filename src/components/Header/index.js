c6bf85e2cce3a84ef914f767500fd7be1ccc6965
Niki Fereidooni <nikifereidooni@gmail.com>
2021-10-28 14:21:20 -0400

    Fixed header issues for mobile, including collapse on select and formatting

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 
 function Header(props) {
+
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <Container>
       <Navbar className="Navbar-main"
@@ -13,18 +16,19 @@ function Header(props) {
         bg="light"
         variant="light"
         fixed="top"
+        expanded={expanded}
       >
-        <Navbar.Brand href="http://www.codebyniki.com">
-          <p className="logo">code by niki</p>
+        <Navbar.Brand href="http://www.codebyniki.com" className="logo">
+          code by niki
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto mt-1 mt-lg-0">
-            <NavLink className="navLink" to="/">Home</NavLink> &nbsp; &nbsp;
-            <NavLink className="navLink" to="/code">Code</NavLink> &nbsp; &nbsp;
-            <NavLink className="navLink" to="/design">Design</NavLink> &nbsp; &nbsp;
-            <NavLink className="navLink" to="/about">About</NavLink> &nbsp; &nbsp;
-            <NavLink className="navLink" to="/contact">Contact</NavLink>
+            <NavLink className="navLink" to="/" onClick={() => setExpanded(false)}>Home</NavLink> &nbsp; &nbsp;
+            <NavLink className="navLink" to="/code" onClick={() => setExpanded(false)}>Code</NavLink> &nbsp; &nbsp;
+            <NavLink className="navLink" to="/design" onClick={() => setExpanded(false)}>Design</NavLink> &nbsp; &nbsp;
+            <NavLink className="navLink" to="/about" onClick={() => setExpanded(false)}>About</NavLink> &nbsp; &nbsp;
+            <NavLink className="navLink" to="/contact" onClick={() => setExpanded(false)}>Contact</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
