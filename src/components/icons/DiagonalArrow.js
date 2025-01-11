076968079643b301ab5b9226021cab0a9483d5d0
Niki Fereidooni <nikifereidooni@Nikis-MacBook-Air.local>
2025-01-10 20:53:49 -0500

    added social icons, added focus states

--- src/components/icons/DiagonalArrow.js
+++ src/components/icons/DiagonalArrow.js
@@ -5,7 +5,7 @@ export default function DiagonalArrow({ className = "" }) {
         height="24px"
         viewBox="0 -960 960 960"
         width="24px"
-        className={`absolute top-3 right-3 text-gray-500 transform transition-transform group-hover:text-pink-300 group-hover:translate-x-1 group-hover:translate-y-[-1px] ${className}`}
+        className={`absolute top-3 right-3 text-gray-500 transform transition-transform group-hover:text-pink-300 group-focus:text-pink-300 group-hover:translate-x-1 group-focus:translate-x-1 group-hover:translate-y-[-1px] group-focus:translate-y-[-1px] ${className}`}
         fill="currentColor"
         aria-hidden="true"
       >
